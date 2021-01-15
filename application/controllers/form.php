d0befc1ade9dc48bae8433ff3a724fca487977f5
bscs17039 <bscs17039@itu.edu.pk>
2021-01-15 19:16:16 +0500

    Update form.php
    /controllers/form.php Line #1395- Line is greater then 80 characters

--- application/controllers/form.php
+++ application/controllers/form.php
@@ -1392,7 +1392,8 @@ class Form extends CI_Controller {
             $data['all_visits_hidden'] = 0;
             $view_list = array();
             foreach ($forms_list as $final_view) {
-                $view_list = array_merge($view_list, array($final_view['form_name'] => $final_view['form_id']));
+                $view_list = array_merge($view_list, 
+                	array($final_view['form_name'] => $final_view['form_id']));
             }
             $view_list = array_flip($view_list);
             $data['form_lists'] = $view_list;
