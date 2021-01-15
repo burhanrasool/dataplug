705e6f42bc3b4cc493f702e39adfd34c83d6323f
desimcnuggets <aiyan.h99@gmail.com>
2021-01-15 18:44:41 +0500

    Update edit.php
    /views/apimaker/edit.php Line #10- Line is greater then 80 characters
    
    /views/apimaker/edit.php Line #17- Line is greater then 80 characters
    
    /views/apimaker/edit.php Line #34- Line is greater then 80 characters
    
    /views/apimaker/edit.php Line #51- Line is greater then 80 characters

--- application/views/apimaker/edit.php
+++ application/views/apimaker/edit.php
@@ -7,14 +7,16 @@
                 <tbody>
                     <tr>
                         <td>
-                            <?php echo form_open(base_url().'edit-new-api/'.$api_id,'id="addform" class="full validate add_task_form " enctype="multipart/form-data"'); ?>
+                            <?php echo form_open(base_url().'edit-new-api/'.$api_id,'id="addform" 
+								class="full validate add_task_form " enctype="multipart/form-data"'); ?>
                             
                             <div class="row">
                                 <label for="d1_textfield">
                                     <strong>API Title</strong>
                                 </label>
                                 <div>
-                                    <input class="required" type="text" name="api_title" id="api_title" value="<?php echo $api_title; ?>"/>
+                                    <input class="required" type="text" name="api_title" 
+										id="api_title" value="<?php echo $api_title; ?>"/>
                                     <span id='availability_status'></span>
                                 </div>
                             </div>
@@ -31,7 +33,8 @@
                                             <?php
                                             foreach($departments as $key=>$val){
                                                 ?>
-                                                <option <?php if($saved_department_id==$val['id']){ echo "selected"; }  ?> value="<?php echo $val['id']; ?>"><?php echo $val['name']; ?></option>
+                                                <option <?php if($saved_department_id==$val['id']){ echo "selected"; }  ?> 
+												value="<?php echo $val['id']; ?>"><?php echo $val['name']; ?></option>
                                             <?php
                                             }
                                             ?>
@@ -48,7 +51,8 @@
                                     <strong>API File (.csv only)</strong>
                                 </label>
                                 <div>
-                                    <input type="file" name="userfile_addapi" id="userfile_addapi" accept="*.csv" onchange="check_file()"/>
+                                    <input type="file" name="userfile_addapi" id="userfile_addapi" 
+										accept="*.csv" onchange="check_file()"/>
                                 </div>
                             </div>
                             <div class="actions">
