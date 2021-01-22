6a8a13ef36de3ec48fffce9d507f45296369d8d5
muzamilarshad <55027684+muzamilarshad@users.noreply.github.com>
2021-01-22 17:15:11 +0500

    Update users.php
    Open Braces corrected on Line 6

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -3,7 +3,8 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class Users extends CI_Controller {
+class Users extends CI_Controller
+{
 
     public function __construct() {
         parent::__construct();
